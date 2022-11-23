6553e673cdde87ff5b0168830497116d97346da2
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-11-24 00:33:23 +0100

    spring boot CRUD Operations - Postman

---
+++ src/main/java/com/cydeo/model/Employee.java
@@ -0,0 +1,28 @@
+package com.cydeo.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+
+
+
+}
