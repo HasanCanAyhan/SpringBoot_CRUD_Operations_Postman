7ac2cdae8a99d568eb4545226735811ca0ecdc48
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-11-24 00:25:19 +0100

    spring boot CRUD Operations - Postman

--- src/main/java/com/cydeo/springboot_crud_operations_postman/SpringBootCrudOperationsPostmanApplication.java
+++ src/main/java/com/cydeo/springboot_crud_operations_postman/SpringBootCrudOperationsPostmanApplication.java
@@ -8,6 +8,8 @@ public class SpringBootCrudOperationsPostmanApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootCrudOperationsPostmanApplication.class, args);
+
+
     }
 
 }
