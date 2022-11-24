bf7cb096da76554d63408e83a77808a4590a33a9
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-11-24 01:16:32 +0100

    spring boot CRUD Operations - Postman

--- src/main/java/com/cydeo/SpringBootCrudOperationsPostmanApplication.java
+++ src/main/java/com/cydeo/SpringBootCrudOperationsPostmanApplication.java
@@ -1,15 +1,43 @@
 package com.cydeo;
 
+import com.cydeo.model.Employee;
+import com.cydeo.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootCrudOperationsPostmanApplication {
+public class SpringBootCrudOperationsPostmanApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootCrudOperationsPostmanApplication.class, args);
 
 
     }
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Override
+    public void run(String... args) throws Exception {
+
+        Employee employee = new Employee();
+        employee.setFirstName("Can");
+        employee.setLastName("Ayhan");
+        employee.setEmailId("c@gmail.com");
+
+        employeeRepository.save(employee);
+
+        Employee employee1 = new Employee();
+        employee1.setFirstName("Ramesh");
+        employee1.setLastName("Cena");
+        employee1.setEmailId("ramesh@gmail.com");
+
+        employeeRepository.save(employee1);
+
+
+    }
+
+
 
 }
---
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -0,0 +1,60 @@
+package com.cydeo.controller;
+
+
+import com.cydeo.exception.ResourceNotFoundException;
+import com.cydeo.model.Employee;
+import com.cydeo.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController //includes @ResponseBody
+@RequestMapping("/api/v1/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+
+    @GetMapping
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    //build create employee rest api
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee){ // @RequestBody converting JSON TO JAVA OBJECT
+        return employeeRepository.save(employee);
+    }
+
+
+    //build get employee by id rest api
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow( () -> new ResourceNotFoundException("Employee not exist with id:" + id));
+
+        return ResponseEntity.ok(employee);
+    }
+
+
+    //build update employee rest api
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails){
+        Employee updateEmployee = employeeRepository.findById(id)
+                .orElseThrow( () -> new ResourceNotFoundException("Employee not exist with id:" + id));
+
+        updateEmployee.setFirstName(employeeDetails.getFirstName());
+        updateEmployee.setLastName(employeeDetails.getLastName());
+        updateEmployee.setEmailId(employeeDetails.getEmailId());
+
+        employeeRepository.save(updateEmployee);
+
+        return ResponseEntity.ok(updateEmployee);
+
+    }
+
+}
